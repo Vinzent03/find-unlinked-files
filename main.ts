3159c78a3367fffdf88e0216be8f66687b23e9da
Vinzent <vinzentwitte@gmail.com>
2020-11-16 20:09:01 +0100

    add option to disable working links

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
-import { Plugin, TFile } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	onload() {
+	settings: Settings;
+	async onload() {
 		console.log('loading ' + this.manifest.name);
+
+		this.settings = await this.loadData() || new Settings();
+
 		this.addCommand({
 			id: 'find-unlinked-files',
 			name: 'Find unlinked files',
@@ -46,17 +50,50 @@ export default class MyPlugin extends Plugin {
 					}
 				});
 				let text = ""
+				let prefix: string;
+				if (this.settings.disableWorkingLinks)
+					prefix = "	"
+				else
+					prefix = ""
 				notLinkedFiles.forEach((file) => {
-					text += "- [[" + this.app.metadataCache.fileToLinktext(file, "/") + "]]\n";
-				})
+					text += prefix + "- [[" + this.app.metadataCache.fileToLinktext(file, "/") + "]]\n";
+				});
 				this.app.vault.adapter.write(outFile, text).then(() => {
 					this.app.workspace.openLinkText(outFile, "/")
-				})
+				});
 			},
 		});
+		this.addSettingTab(new SettingsTab(this.app, this))
 	}
 
 	onunload() {
 		console.log('unloading ' + this.manifest.name);
+	}
+
+}
+class Settings {
+	disableWorkingLinks: boolean = false;
+}
+
+class SettingsTab extends PluginSettingTab {
+	plugin: MyPlugin;
+	constructor(app: App, plugin: MyPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+		containerEl.empty();
+		containerEl.createEl("h2", { text: this.plugin.manifest.name })
+
+		new Setting(containerEl)
+			.setName('Disable working links')
+			.setDesc('Indent lines to disable the link and to clean up the graph view')
+			.addToggle(cb => cb.onChange(value => {
+				this.plugin.settings.disableWorkingLinks = value;
+				this.plugin.saveData(this.plugin.settings);
+			}
+			).setValue(this.plugin.settings.disableWorkingLinks));
 	}
 }
