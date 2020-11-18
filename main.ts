f60b071e4d6569a28cf223d177c7fedfdd8cc7ed
Vinzent <vinzentwitte@gmail.com>
2020-11-18 14:45:18 +0100

    add option to ignore files and directories

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
-import { App, getLinkpath, iterateCacheRefs, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { App, getLinkpath, iterateCacheRefs, normalizePath, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 interface Settings {
 	disableWorkingLinks: boolean;
+	directoriesToIgnore: string[];
+	filesToIgnore: string[];
 }
 export default class MyPlugin extends Plugin {
 	settings: Settings;
 	async onload() {
-		console.log('loading ' + this.manifest.name);
-
-		this.settings = await this.loadData() ?? { disableWorkingLinks: false }
+		console.log('loading ' + this.manifest.name + " plugin");
+		let tempData: Settings = await this.loadData();
+		this.settings = {
+			disableWorkingLinks: tempData?.disableWorkingLinks ?? false,
+			directoriesToIgnore: tempData?.directoriesToIgnore ?? [],
+			filesToIgnore: tempData?.filesToIgnore ?? [],
+		}
 
 		this.addCommand({
 			id: 'find-unlinked-files',
@@ -36,9 +42,18 @@ export default class MyPlugin extends Plugin {
 						return
 					if (["css"].contains(file.extension))
 						return
-					if (!links.contains(file.path)) {
-						notLinkedFiles.push(file)
-					}
+					let ignoreBecauseOfDirectory = false;
+					this.settings.directoriesToIgnore.forEach(value => {
+						if (file.path.startsWith(value) && value.length != 0)
+							ignoreBecauseOfDirectory = true;
+					})
+					if (ignoreBecauseOfDirectory)
+						return
+					if (this.settings.filesToIgnore.contains(file.path))
+						return
+					if (links.contains(file.path))
+						return
+					notLinkedFiles.push(file)
 				});
 				let text = ""
 				let prefix: string;
@@ -57,7 +72,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	onunload() {
-		console.log('unloading ' + this.manifest.name);
+		console.log('unloading ' + this.manifest.name + " plugin");
 	}
 
 }
@@ -82,5 +97,40 @@ class SettingsTab extends PluginSettingTab {
 				this.plugin.saveData(this.plugin.settings);
 			}
 			).setValue(this.plugin.settings.disableWorkingLinks));
+
+		new Setting(containerEl)
+			.setName("Directories to ignore.")
+			.setDesc("Add each directory path in a new line")
+			.addTextArea(cb => cb
+				.setPlaceholder("Directory/Subdirectory")
+				.setValue(this.plugin.settings.directoriesToIgnore.join("\n"))
+				.onChange((value) => {
+					let paths = value.trim().split("\n").map(value => formatPath(value, true));
+					this.plugin.settings.directoriesToIgnore = paths;
+					this.plugin.saveData(this.plugin.settings);
+				}));
+
+
+		new Setting(containerEl)
+			.setName("Files to ignore.")
+			.setDesc("Add each file path in a new line (with file extension!)")
+			.addTextArea(cb => cb
+				.setPlaceholder("Directory/file.md")
+				.setValue(this.plugin.settings.filesToIgnore.join("\n"))
+				.onChange((value) => {
+					let paths = value.trim().split("\n").map(value => formatPath(value, false));
+					this.plugin.settings.filesToIgnore = paths;
+					this.plugin.saveData(this.plugin.settings);
+				}));
+
+		function formatPath(path: string, addDirectorySlash: boolean): string {
+			if (path.length == 0)
+				return path;
+			path = normalizePath(path);
+			if (addDirectorySlash)
+				return path + "/"
+			else
+				return path
+		}
 	}
 }
